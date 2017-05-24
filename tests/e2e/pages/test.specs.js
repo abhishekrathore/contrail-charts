86a835ff529eb48df394eeb29de3cd5a7277ea76
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-24 15:39:02 +0530

    one test case

--- tests/e2e/pages/test.specs.js
+++ tests/e2e/pages/test.specs.js
@@ -1,14 +1,14 @@
 /* global describe, it, expect */
 describe('Testing jasmine on circle CI', function () {
-  beforeAll(function (done) {
-    //  elusive browser object, I will find where you're defined!
-    browser
-      .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
-      .call(done);
-  });
-  afterAll(function (done) {
-    browser.end(done);
-  });
+  // beforeAll(function (done) {
+  //   //  elusive browser object, I will find where you're defined!
+  //   browser
+  //     .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
+  //     .call(done);
+  // });
+  // afterAll(function (done) {
+  //   browser.end(done);
+  // });
   describe('Jasmine Test cases', () => {
     it('jasmine test case to be passed', (done) => {
       browser
